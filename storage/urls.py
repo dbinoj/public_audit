ece4ad623e825e139cdf77d924b5eb454798af82
Binoj D <dbinoj@gmail.com>
2015-03-17 21:28:20 +0530

    added storage urls

---
+++ storage/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, url
+
+from storage import views
+
+urlpatterns = patterns('',
+    # /storage/
+    url(r'^$', views.index, name='index'),
+    # /storage/file/5/
+    url(r'^file/(?P<file_id>\d+)/$', views.file_detail, name='file_detail'),
+
+)
