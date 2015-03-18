152e82808ebbaa646aecec8828a7a4b8aacef940
Kiruthika Velusamy <kiruthika94velusamy@gmail.com>
2015-03-18 09:33:04 +0000

    added storage template

--- storage/views.py
+++ storage/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from storage.models import ClientFile
 # Create your views here.
+def index(request):
+    file_list = ClientFile.objects.order_by('ts')
+    context = {'file_list': file_list}
+    return render(request, 'storage/index.html', context)
