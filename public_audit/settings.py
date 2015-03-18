cc1445a0521de13f7968b16a43b8954575c0e7a0
Binoj D <dbinoj@gmail.com>
2015-03-18 14:14:39 +0530

    Closed static tag in base.html at the wrong place.

--- public_audit/settings.py
+++ public_audit/settings.py
@@ -91,5 +91,5 @@ STATICFILES_DIRS = (
 )
 
 TEMPLATE_DIRS = [
-    os.path.join(BASE_DIR, 'templates'),
+    os.path.join(BASE_DIR, "templates"),
 ]
--- public_audit/urls.py
+++ public_audit/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^client/', include('client.urls', namespace="client")),
-    url(r'^storage/', include('storage.urls', namespace="storage")),
+    #url(r'^storage/', include('storage.urls', namespace="storage")),
     url(r'^admin/', include(admin.site.urls)),
 )
