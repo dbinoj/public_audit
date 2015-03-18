b9ea4a4697d7a18f956b42e5d9c299d727d46034
Kiruthika Velusamy <kiruthika94velusamy@gmail.com>
2015-03-18 11:21:56 +0000

    added a tpa template with stub views

--- tpa/views.py
+++ tpa/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    #file_id = FileMeta.objects.order_by('ts')
+    #context = {'file_list': file_list}
+    return render(request, 'tpa/index.html')
