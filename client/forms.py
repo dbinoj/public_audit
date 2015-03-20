163ee510246ba6401353e14b9c55d24810749ee4
Binoj D <dbinoj@gmail.com>
2015-03-20 12:42:45 +0530

    learning more form stuff.. And 'how not to solve merge conflicts' -_-

--- client/forms.py
+++ client/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class FileUploadForm(forms.Form):
-    file_field = forms.FileField()
+    file_field = forms.FileField(label="")
--- client/views.py
+++ client/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, get_object_or_404
 
 from client.models import FileMeta
 
+from .forms import FileUploadForm
 # Create your views here.
 
 
 def index(request):
+    form = FileUploadForm()
     file_list = FileMeta.objects.order_by('ts')
-    context = {'file_list': file_list}
+    context = {'file_list': file_list, "form": form}
     return render(request, 'client/index.html', context)
 
 def file_detail(request, file_id):
