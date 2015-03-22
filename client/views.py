d60edb41db735d0190d2b98acbcc8c241bb8ef9c
Binoj D <dbinoj@gmail.com>
2015-03-22 22:58:34 +0530

    improvement with files

--- client/views.py
+++ client/views.py
@@ -9,7 +9,9 @@ from .forms import FileUploadForm
 def index(request):
     form = FileUploadForm(request.POST or None, request.FILES or None)
     if form.is_valid():
-        print "Valid Form"
+        file = request.FILES['file_field']
+        file_size = len(file)
+        file_name = file.name
     file_list = FileMeta.objects.order_by('ts')
     context = {'file_list': file_list, "form": form}
     return render(request, 'client/index.html', context)
