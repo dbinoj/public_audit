bf9f6002d84b6f24db227103598f573951ae58f2
Kiruthika Velusamy <kiruthika94velusamy@gmail.com>
2015-03-23 12:55:50 +0000

    admin files hav been changed and added models

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+
+
 # Register your models here.
+from . import models
+
+admin.site.register(models.ClientFile)
+admin.site.register(models.FileBlock)
+admin.site.register(models.AuditResponse)
