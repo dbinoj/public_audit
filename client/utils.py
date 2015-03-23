b929ddc7ccc0f16c6b43eb2ce7141e4bac71bd8d
Kiruthika Velusamy <kiruthika94velusamy@gmail.com>
2015-03-23 06:07:32 +0000

    changed blocks key to sequential nunmbers from start bytes

--- client/utils.py
+++ client/utils.py
@@ -5,8 +5,10 @@ def split_files(file, chunkSize):
     if(bytes%chunkSize):
         noOfChunks += 1
     blocks = {}
+    b_count = 1
     for i in range(0, bytes+1, chunkSize):
-        file_chunk = { "b_%s" % i: file[i:i + chunkSize] }
+        file_chunk = { "b_%s" % b_count: file[i:i + chunkSize] }
+	b_count += 1
         blocks.update(file_chunk)
 
     return blocks
