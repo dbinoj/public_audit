dc4c4d76704ad888c54d04736aff7e224332a848
Kiruthika Velusamy <kiruthika94velusamy@gmail.com>
2015-03-24 06:00:48 +0000

    changed admin name from django administration to public_audit

--- client/admin.py
+++ client/admin.py
@@ -5,3 +5,4 @@ from . import models
 
 admin.site.register(models.FileMeta)
 admin.site.register(models.FileAudit)
+admin.site.site_header = 'Public_audit'
--- tpa/admin.py
+++ tpa/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 
 # Register your models here.
 from . import models
-
+admin.site.site_header = 'Public_audit'
 admin.site.register(models.AuditRequest)
