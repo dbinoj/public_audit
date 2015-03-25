5765ca127f39e5611418b03d7415ae777e91bf5c
Kiruthika Velusamy <kiruthika94velusamy@gmail.com>
2015-03-25 11:03:34 +0000

    url bug fixed

--- tpa/urls.py
+++ tpa/urls.py
@@ -6,8 +6,8 @@ urlpatterns = patterns('',
     # /tpa/
     url(r'^$', views.index, name='index'),
     # /tpa/5/rts/
-    url(r'^tpa/(?P<file_id>\d+)/rts/$', views.file_requestto_server, name='file_requestto_server'),
+    url(r'^(?P<file_id>\d+)/rts/$', views.file_requestto_server, name='file_requestto_server'),
     # /tpa/5/verify/
-    url(r'^tpa/(?P<file_id>\d+)/verify/$', views.verify, name='verify'),
+    url(r'^(?P<file_id>\d+)/verify/$', views.verify, name='verify'),
 
 )
