410e5c1c8855f3a9c98bd406382e367c9d6f7904
Binoj D <dbinoj@gmail.com>
2015-03-25 14:25:35 +0530

    Fixed generating hash so that it is always constant.

--- client/views.py
+++ client/views.py
@@ -15,7 +15,7 @@ from Crypto.Hash import SHA256
 from rsa.bigfile import *
 from functools import partial
 
-import os, tempfile
+import os, tempfile, collections
 import base64
 import cStringIO
 import hashlib
@@ -34,12 +34,12 @@ def index(request):
         file = request.FILES['file_field']
         file_size = file.size
         file_name = file.name
-        md5 = hashlib.new('md5')
-        sha1 = hashlib.new('sha1')
+        #md5 = hashlib.new('md5')
+        #sha1 = hashlib.new('sha1')
         sha256 = SHA256.new()
         for chunk in iter(partial(file.read, 8192), ''):
-            md5.update(chunk)
-            sha1.update(chunk)
+            #md5.update(chunk)
+            #sha1.update(chunk)
             sha256.update(chunk)
         
         # The above with block sends the file pointer to the end of the file
@@ -47,8 +47,8 @@ def index(request):
 
         RSAkey = RSA.generate(1024)
         AESkey = rsa.randnum.read_random_bits(128)
-        file_md5 = md5.hexdigest()
-        file_sha1 = sha1.hexdigest()
+        #file_md5 = md5.hexdigest()
+        #file_sha1 = sha1.hexdigest()
         signer = PKCS1_v1_5.new(RSAkey)
         signature = signer.sign(sha256)
         signature_b64 = base64.b64encode(signature)
@@ -64,8 +64,8 @@ def index(request):
             size=file_size, 
             private_key=RSAkey.exportKey(), 
             aes_key=AESkey, 
-            hash_sha1=file_sha1, 
-            hash_md5=file_md5, 
+            hash_sha1='', 
+            hash_md5='', 
             signature=signature_b64
         )
 
@@ -74,13 +74,16 @@ def index(request):
             size=file_size,
             signature=signature_b64
         )
-        filemeta.storage_file_id = clientfile.id
-        filemeta.save()
 
         blocks = split_files(file.read(), STORAGE_BLOCK_SIZE)
         blocks_count = len(blocks)
+        # Python Dicts are not sorted. Sorting using collections.OrderedDict
+        # Must sort so that we get same hash everytime.
+        blocks = collections.OrderedDict(sorted(blocks.items()))
         clientfile.blocks = blocks_count
         clientfile.save()
+        enc_md5 = hashlib.new('md5')
+        enc_sha1 = hashlib.new('sha1')
         blocks_enc = {}
         for key, value in blocks.iteritems():
             plain_value = tempfile.NamedTemporaryFile()
@@ -94,25 +97,31 @@ def index(request):
             
             encrypt_file(AESkey, plain_value.name, block_path)
             encrypted_block = open(block_path, 'r')
-            block_md5 = hashlib.md5(encrypted_block.read()).hexdigest()
-            encrypted_block.seek(0)
-            block_sha1 = hashlib.sha1(encrypted_block.read()).hexdigest()
-            encrypted_block.seek(0)
-            block_sha256 = SHA256.new(encrypted_block.read())
+            block_contents = encrypted_block.read()
+            encrypted_block.close()
+            block_md5 = hashlib.md5(block_contents).hexdigest()
+            block_sha1 = hashlib.sha1(block_contents).hexdigest()
+            enc_md5.update(block_contents)
+            enc_sha1.update(block_contents)
+            block_sha256 = SHA256.new(block_contents)
             block_signature = signer.sign(block_sha256)
             block_signature_b64 = base64.b64encode(block_signature)
             fileblock = FileBlock.objects.create(
                 file_id=clientfile,
                 path=block_path,
-                #tag_id=clientfile.id,
                 hash_sha1=block_sha1,
                 hash_md5=block_md5,
                 block_size=os.path.getsize(block_path),
                 signature=block_signature_b64
             )
-            encrypted_block.close()
             if plain_value.closed == False:
                 plain_value.close()
+        enc_file_md5 = enc_md5.hexdigest()
+        enc_file_sha1 = enc_sha1.hexdigest()
+        filemeta.storage_file_id = clientfile.id
+        filemeta.hash_md5 = enc_file_md5
+        filemeta.hash_sha1 = enc_file_sha1
+        filemeta.save()
 
     file_list = FileMeta.objects.order_by('ts')
     context = {'file_list': file_list, "form": form}
