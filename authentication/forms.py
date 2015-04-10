76634ba393d98bb3d1d8deea2e795094455f6815
Kiruthika Velusamy <kiruthika94velusamy@gmail.com>
2015-04-10 07:44:48 +0000

    login system worked for client

--- authentication/forms.py
+++ authentication/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-class  login_userForm(forms.Form):
-    username        = forms.CharField(label=(u'UserName'))
-    password        = forms.CharField(label=(u'Password'), widget=forms.PasswordInput(render_value=False))
-        
+class  LoginUserForm(forms.Form):
+    username = forms.CharField(label=(u'UserName'),widget=forms.TextInput(attrs={'class': "form-control",'autofocus':'autofocus','placeholder':'Username','required':'required'}))
+    password = forms.CharField(label=(u'Password'),widget=forms.PasswordInput(attrs={'class': "form-control",'placeholder':'Password','required':'required'}))
--- authentication/views.py
+++ authentication/views.py
@@ -1,16 +1,32 @@
 from django.shortcuts import render, render_to_response, redirect
+from django.http import HttpResponseRedirect
 from django.template import RequestContext
-from django.contrib.auth import logout
-from authentication.forms import login_userForm
+from django.contrib.auth import authenticate, login, logout
+from authentication.forms import LoginUserForm
+
+
  
 def index(request):
     if request.user.is_authenticated():
         return redirect('client:index')
+
     else:
         return redirect('authentication:login_user')
 
 def login_user(request):
-    return render_to_response('authentication/login.html')
+    form = LoginUserForm(request.POST or None)
+    if form.is_valid():
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return render(request,'client/index.html')
+        else:
+            return render(request, 'authentication/login.html', {"form": form})
+               
+    else:
+        return render(request, 'authentication/login.html', {"form": form})
 
 def logout_user(request):
     logout(request)
